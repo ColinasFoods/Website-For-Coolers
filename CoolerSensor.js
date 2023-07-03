9aed2a9f0a54a5c1fe074a6a0c6a1732d545e2ba
Mau38 <maudiaz0109@gmail.com>
2023-07-03 15:33:33 -0500

    added new object for sensors and added sensors map

--- CoolerSensor.js
+++ CoolerSensor.js
@@ -1,4 +1,9 @@
 class CoolerSensor {
-    constructor(id, temp, doorOpen)
-    // constructor(id, temp, doorOpen, doorTimeOpen)
+    constructor(id, temp, doorOpen, doorTimeOpen) {
+        this.id = id;
+        this.temp = temp;
+        this.doorOpen = doorOpen;
+        this.doorTimeOpen = doorTimeOpen;
+    }
+    
 }
