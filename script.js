b21ecd4553a53d1d13e04e6725d0e4c5e284e191
Gulin <gulinisawesome@gmail.com>
2023-07-03 15:43:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ fetch('http://localhost:3000/temperature' )
         data = json;
         Object.values(data).forEach(element => {
             var temp = Object.values(element);
-            
+            console.log(temp[1], temp[2], temp[3])
             sensorMap.set(temp[0], new CoolerSensor(temp[0], temp[1], temp[2], temp[3]))
         });
     });
     
-console.log(sensorMap)
+
