7d01dda51d362dcc440743a02836ded96edbc7df
Mau38 <maudiaz0109@gmail.com>
2023-07-24 11:40:25 -0500

    idk

--- CoolerSensor.js
+++ CoolerSensor.js
@@ -1,9 +1,10 @@
 class CoolerSensor {
-    constructor(id, temp, doorOpen, doorTimeOpen, time) {
+    constructor(id, temp, doorOpen, doorTimeOpen, previousTemps, time) {
         this.id = id;
         this.temp = temp;
         this.doorOpen = doorOpen;
         this.doorTimeOpen = doorTimeOpen;
+        this.previousTemps = previousTemps;
         this.time = time;
     }
     
