07387289814df0dc441dde5847acac7ab77f9d34
PatricioSal <pato.sallopez14@gmail.com>
2023-07-28 12:22:28 -0500

    Added door open duration

--- CoolerSensor.js
+++ CoolerSensor.js
@@ -1,10 +1,11 @@
 class CoolerSensor {
-    constructor(id, temp, doorOpen, doorTimeOpen, previousTemps, time) {
+    constructor(id, temp, doorOpen, doorTimeOpen, previousTemps, previousDoors, time) {
         this.id = id;
         this.temp = temp;
         this.doorOpen = doorOpen;
         this.doorTimeOpen = doorTimeOpen;
         this.previousTemps = previousTemps;
+        this.previousDoors = previousDoors;
         this.time = time;
     }
     
